b677f3d5432b2c400b5a36b5a55a1c325355c8c9
S. Lakshmidhar Reddy <sanelakshmidharreddy@gmail.com>
2026-05-04 05:34:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
-
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"), // ✅ FIXED
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== "true",
-    },
-  };
+  },
 });
